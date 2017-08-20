585705b4dd1e364361ecbd82a975ee95d9685928
Fritz Lin <uxfritz@163.com>
2017-08-20 23:05:09 +0800

    wip(clipboard) added (internal) copy(y), paste(p) shortcut native methods not working, consider a chrome extension or electron app https://stackoverflow.com/questions/39193878/javascript-execcommandpaste-not-working

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,8 @@ import text from '~/assets/text'
 export default {
   data () {
     return {
+      clipboard: [],
+      mode: 'normal',
       text: text.trim(),
       range: { start: 0, end: 0 }
     }
@@ -23,7 +25,47 @@ export default {
       return this.text.split('')
     }
   },
+  mounted () {
+    window.addEventListener('keydown', this.keyDown)
+  },
+  beforeDestory () {
+    window.removeEventListener('keydown', this.keyDown)
+  },
   methods: {
+    insert (text, prepend = false) {
+      let selection = window.getSelection()
+      let range = selection.getRangeAt(0)
+      let i = +range.endContainer.parentNode.dataset.index + 1
+      if (prepend) i -= 1 
+      this.text = this.text.slice(0, i) + text + this.text.slice(i)
+    },
+    paste (prepend = false) {
+      let text = this.clipboard[0] || ''
+      this.insert(text, prepend)
+    },
+    copy () {
+      document.execCommand('copy')
+      let selection = window.getSelection()
+      let range = selection.getRangeAt(0)
+      let text = range.toString()
+      this.clipboard.unshift(text)
+    },
+    keyDown (e) {
+      let c = e.keyCode
+      if (this.mode === 'insert') {
+        return
+      }
+      // e.preventDefault()
+      if (this.mode === 'normal') {
+        if (c === 'Y'.charCodeAt(0)) {
+          return this.copy()
+        }
+        if (c === 'P'.charCodeAt(0)) {
+          let prepend = e.shiftKey
+          return this.paste(prepend)
+        }
+      }
+    },
     charClick (i) {
       let range = document.createRange()
       let span = this.$refs[`char-${i}`][0]
